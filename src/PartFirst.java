298858f1ad7a2b8b18f0cd455643c59a458be369
Florencia <floppyespinosa@gmail.com>
2021-02-09 14:06:44 -0500

    I added logout 13 - 14 - 15

--- src/PartFirst.java
+++ src/PartFirst.java
@@ -76,5 +76,32 @@ public class PartFirst {
         driver.findElement(By.cssSelector("body[id='tinymce']")).click();
         driver.findElement(By.cssSelector("body[id='tinymce']")).sendKeys("Promotion was awarded to Anna on 1/7/2020");
 
+
+
+
+
+
+        //logout
+
+        driver.findElement(By.id("account-job")).click();
+        driver.findElement(By.id("logoutLink")).click();
+
+        // 13 Log in as Administrator
+
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+        //14 Open Admin -> Announcements -> News
+
+        driver.findElement(By.id("menu_admin_viewAdminModule")).click();
+        driver.findElement(By.id("menu_news_Announcements")).click();
+        driver.findElement(By.id("menu_news_viewNewsList")).click();
+
+        // 15 Check the item and delete it
+
+        String text = driver.findElement(By.xpath("//*[@id=\"resultTable\"]/tbody/tr[1]/td[2]/a")).getText();
+        driver.findElement(By.xpath("(//tr[@class='dataRaw']//td//label)[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"frmList_ohrmListComponent_Menu\"]/i")).click();
+        driver.findElement(By.id("newsDelete")).click();
+        driver.findElement(By.id("news-delete-button")).click();
     }
 }
